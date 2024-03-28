f61acc4eac5cad43624ea50f47e93f176412f538
V ARAVIND <aravindaravindachari55@gmail.com>
2024-03-28 22:53:56 +0530

    step2

---
+++ Step2.java
@@ -0,0 +1,25 @@
+package jdbc1;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Step2 {
+
+	public static void main(String[] args) {
+		//load and register
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// establish the connection
+			String url="jdbc:mysql://localhost:3306/db1";
+			String user="root";
+			String pwd="root";
+		DriverManager.getConnection(url, user, pwd);
+		System.out.println("connection done");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
