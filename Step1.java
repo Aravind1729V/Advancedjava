3afec9eaaf4dcd14e55502a364ef691ed9c8f4cb
V ARAVIND <aravindaravindachari55@gmail.com>
2024-03-29 12:30:47 +0530

    Step1.java

--- Step1.java
+++ Step1.java
@@ -1,11 +1,20 @@
 package jdbc1;
 
+//import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//import com.mysql.cj.jdbc.*;
+
+
 public class Step1 {
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		//load and Register -------driver
 		//2 ways
-		Class.forName("com.mysql.cj.jdbc.Driver");
+		//Class.forName("com.mysql.cj.jdbc.Driver");
+//		Driver d=new com.mysql.cj.jdbc.Driver();
+//		DriverManager.registerDriver(d);
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 		System.out.println("done");
 	}
 
--- Step2.java
+++ Step2.java
@@ -6,15 +6,16 @@ import java.sql.SQLException;
 public class Step2 {
 
 	public static void main(String[] args) {
-		//load and register
+		// load and register
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			// establish the connection
-			String url="jdbc:mysql://localhost:3306/db1";
-			String user="root";
-			String pwd="root";
-		DriverManager.getConnection(url, user, pwd);
-		System.out.println("connection done");
+//			String url="jdbc:mysql://localhost:3306/db1";
+//			String user="root";
+//			String pwd="root";
+//		DriverManager.getConnection(url, user, pwd);
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/db1?user=root&password=root");
+			System.out.println("connection done");
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
